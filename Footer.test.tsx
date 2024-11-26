85f1f22207023f8aebf07750ac633e9e0902eae0
Rahul Kumar Mishra <74014846+I-rahulmishra@users.noreply.github.com>
2024-11-26 13:55:24 +0530

    Update Footer.test.tsx

--- Footer.test.tsx
+++ Footer.test.tsx
@@ -1,154 +1,93 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { useSelector } from "react-redux";
 import Footer from "./Footer";
 import { CONSTANTS } from "../../../utils/common/constants";
-import { getUrl } from "../../../utils/common/change.utils";
-
-// Mock useSelector and getUrl
-jest.mock("react-redux", () => ({
-  useSelector: jest.fn(),
-}));
-
-jest.mock("../../../utils/common/change.utils", () => ({
-  getUrl: {
-    getUrlEndPoint: jest.fn(),
-  },
-}));
-
-const mockProps = {
-  backHandler: jest.fn(),
-  validateNxt: "validate-class",
-  journeyType: "NTC",
-  uploadJourney: false,
-  otherMyinfo: false,
-};
 
 describe("Footer Component", () => {
-  let wrapper: any;
-
-  const mockStageSelector = [
-    {
-      stageId: CONSTANTS.STAGE_NAMES.BD_3,
-      stageInfo: {
-        products: [{ product_category: "CC", product_type: "280" }],
-      },
-    },
-  ];
+  let mockBackHandler;
+  let mockProps;
 
   beforeEach(() => {
-    (useSelector as jest.Mock).mockImplementation((callback) =>
-      callback({
-        stages: {
-          stages: mockStageSelector,
+    mockBackHandler = jest.fn();
+    mockProps = {
+      backHandler: mockBackHandler,
+      validateNxt: "valid",
+      journeyType: "NTC",
+      otherMyinfo: false,
+      uploadJourney: false,
+    };
+  });
+
+  it("should render the Footer component without crashing", () => {
+    const wrapper = shallow(<Footer {...mockProps} />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it("should render the 'Back' button when conditions are met", () => {
+    const wrapper = shallow(<Footer {...mockProps} />);
+    // Test when back button should be visible
+    expect(wrapper.find(".back").length).toBe(1);
+  });
+
+  it("should not render the 'Back' button when conditions are not met", () => {
+    mockProps.otherMyinfo = true; // simulate condition where back button should not render
+    const wrapper = shallow(<Footer {...mockProps} />);
+    expect(wrapper.find(".back").length).toBe(0);
+  });
+
+  it("should call backHandler when 'Back' button is clicked", () => {
+    const wrapper = shallow(<Footer {...mockProps} />);
+    wrapper.find(".back").simulate("click");
+    expect(mockBackHandler).toHaveBeenCalledTimes(1);
+  });
+
+  it("should render the 'Continue' button with correct text for 'Agree and Submit' when stageId is 'rp' or uploadJourney is true", () => {
+    mockProps.journeyType = "NTC";
+    mockProps.uploadJourney = true; // simulate uploadJourney as true
+    const wrapper = shallow(<Footer {...mockProps} />);
+    expect(wrapper.find("button.continue").text()).toBe("Agree and Submit");
+  });
+
+  it("should render the 'Continue' button with correct text for 'Continue' when not in 'rp' stage and uploadJourney is false", () => {
+    mockProps.uploadJourney = false;
+    const wrapper = shallow(<Footer {...mockProps} />);
+    expect(wrapper.find("button.continue").text()).toBe("Continue");
+  });
+
+  it("should show spinner when ctaSpinner is true", () => {
+    mockProps.journeyType = "NTC";
+    mockProps.uploadJourney = false;
+    const wrapper = shallow(<Footer {...mockProps} />);
+    wrapper.setState({ ctaSpinner: true });
+    expect(wrapper.find(".circle-spinner").length).toBe(1);
+  });
+
+  it("should hide spinner when ctaSpinner is false", () => {
+    mockProps.journeyType = "NTC";
+    mockProps.uploadJourney = false;
+    const wrapper = shallow(<Footer {...mockProps} />);
+    wrapper.setState({ ctaSpinner: false });
+    expect(wrapper.find(".circle-spinner").length).toBe(0);
+  });
+
+  it("should correctly determine the back button visibility based on stageSelector and props", () => {
+    const mockStageSelector = [
+      {
+        stageId: CONSTANTS.STAGE_NAMES.BD_3,
+        stageInfo: {
+          products: [{ product_category: "CC" }],
         },
-      })
-    );
-    (getUrl.getUrlEndPoint as jest.Mock).mockReturnValue("acknowledge");
-    wrapper = shallow(<Footer {...mockProps} />);
+      },
+    ];
+    mockProps.journeyType = "NTC";
+    const wrapper = shallow(<Footer {...mockProps} />);
+    wrapper.setProps({ stageSelector: mockStageSelector });
+    expect(wrapper.find(".back").length).toBe(0); // Back button should not be visible based on condition
   });
 
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it("should render the Footer component", () => {
-    expect(wrapper.exists(".footer")).toBe(true);
-  });
-
-  describe("Back button behavior", () => {
-    it("should display the back button when backBtn state is true", () => {
-      wrapper.setState({ backBtn: true });
-      expect(wrapper.find(".back").exists()).toBe(true);
-    });
-
-    it("should not display the back button when backBtn state is false", () => {
-      wrapper.setState({ backBtn: false });
-      expect(wrapper.find(".back").exists()).toBe(false);
-    });
-
-    it("should call backHandler when the back button is clicked", () => {
-      wrapper.setState({ backBtn: true });
-      wrapper.find(".back").simulate("click");
-      expect(mockProps.backHandler).toHaveBeenCalled();
-    });
-
-    it("should set backBtn to false for specific journeyType and stage conditions", () => {
-      const customStages = [
-        {
-          stageId: CONSTANTS.STAGE_NAMES.LD_1,
-          stageInfo: {
-            products: [{ product_type: "280" }],
-          },
-        },
-      ];
-      (useSelector as jest.Mock).mockReturnValue({ stages: { stages: customStages } });
-      wrapper = shallow(<Footer {...mockProps} />);
-      expect(wrapper.state("backBtn")).toBe(false);
-    });
-  });
-
-  describe("CTA Spinner behavior", () => {
-    it("should set ctaSpinner to true when urlEndPoint matches 'acknowledge' or 'preserve'", () => {
-      (getUrl.getUrlEndPoint as jest.Mock).mockReturnValue("acknowledge");
-      wrapper = shallow(<Footer {...mockProps} />);
-      expect(wrapper.state("ctaSpinner")).toBe(true);
-    });
-
-    it("should set ctaSpinner to false for other urlEndPoint values", () => {
-      (getUrl.getUrlEndPoint as jest.Mock).mockReturnValue("otherEndpoint");
-      wrapper = shallow(<Footer {...mockProps} />);
-      expect(wrapper.state("ctaSpinner")).toBe(false);
-    });
-  });
-
-  describe("Continue button behavior", () => {
-    it("should display 'Agree and Submit' when ctaSpinner is true or uploadJourney is true", () => {
-      wrapper.setState({ ctaSpinner: true });
-      expect(wrapper.find("button").text()).toContain("Agree and Submit");
-
-      wrapper.setProps({ uploadJourney: true });
-      wrapper.setState({ ctaSpinner: false });
-      expect(wrapper.find("button").text()).toContain("Agree and Submit");
-    });
-
-    it("should display 'Continue' for other conditions", () => {
-      wrapper.setState({ ctaSpinner: false });
-      expect(wrapper.find("button").text()).toContain("Continue");
-    });
-
-    it("should apply the correct class for the continue button based on props.validateNxt", () => {
-      expect(wrapper.find("button").hasClass("validate-class")).toBe(true);
-    });
-  });
-
-  describe("Other Props Handling", () => {
-    it("should handle otherMyinfo prop to determine backBtn state", () => {
-      wrapper.setProps({ otherMyinfo: true });
-      expect(wrapper.state("backBtn")).toBe(true);
-
-      wrapper.setProps({ otherMyinfo: false });
-      expect(wrapper.state("backBtn")).toBe(false);
-    });
-  });
-
-  describe("Stage Selector", () => {
-    it("should set backBtn to false if the first stageId matches specific constants", () => {
-      const customStages = [
-        { stageId: CONSTANTS.STAGE_NAMES.SSF_1, stageInfo: { products: [] } },
-      ];
-      (useSelector as jest.Mock).mockReturnValue({ stages: { stages: customStages } });
-      wrapper = shallow(<Footer {...mockProps} />);
-      expect(wrapper.state("backBtn")).toBe(false);
-    });
-
-    it("should set backBtn to true if conditions do not match for stageSelector", () => {
-      const customStages = [
-        { stageId: "OTHER_STAGE", stageInfo: { products: [] } },
-      ];
-      (useSelector as jest.Mock).mockReturnValue({ stages: { stages: customStages } });
-      wrapper = shallow(<Footer {...mockProps} />);
-      expect(wrapper.state("backBtn")).toBe(true);
-    });
+  it("should correctly set back button visibility when otherMyinfo is true", () => {
+    mockProps.otherMyinfo = true;
+    const wrapper = shallow(<Footer {...mockProps} />);
+    expect(wrapper.find(".back").length).toBe(0);
   });
 });
